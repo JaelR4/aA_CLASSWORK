a727760e20f77736f268e61d53aba96a15969a0e
Jael Rodriguez <jaelrodriguez@Jaels-MacBook-Pro.local>
2022-09-27 18:59:09 -0400

    notes

---
+++ week1/day2/classworkw1d2.js
@@ -0,0 +1,33 @@
+
+console.log("something".indexOf('m'))
+
+if(!0){
+    console.log('intheif')
+} else {
+    console.log('intheelse')
+}
+
+let num=20;
+if(num=30){
+    console.log('intheif')
+}else{
+    console.log("intheelse")
+}
+
+function foo(){
+    return "bar";
+}
+console.log(foo())
+console.log(foo)
+
+function foo(p1,p2){
+return p1 +" "+ p2
+}
+console.log(foo("jael" , "rod"))
+
+let foo1 = function(){
+    return 'bar'
+}
+console.log(foo1())
+
+
