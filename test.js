356bed31b606a3b866e0bfb49d66d0a34bd27c9d
Jael Rodriguez <jaelrodriguez@Jaels-MacBook-Pro.local>
2022-09-26 20:36:37 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,72 @@
+console.log ('hello world')
+// node TextDecoderStream.js
+console.log(2)
+console.log(2+2)
+console.log(2*5)
+console.log(10-3)
+console.log(10/3)
+console.log(10%3)
+console.log(9%2)
+console.log(10%2)
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false | false)
+console.log(true & true)
+console.log(true & false)
+console.log(false & true)
+console.log(false & false)
+console.log(!true)
+console.log(!!true)
+console.log(!false)
+console.log(10>10)
+console.log(10<20)
+console.log(25>=5)
+console.log(25>=25)
+console.log(12===11)
+console.log(12!==11)
+console.log(12=="12")
+console.log('anything')
+let str= "hello"
+console.log(str[1])
+console.log("hello"[1])
+console.log("hello".length)
+console.log("hello".indexOf('h'))
+console.log("hello".indexOf('l'))
+console.log("hello".indexOf('x'))
+console.log("hello".indexOf('hell'))
+console.log('race'.concat("car"))
+console.log('race'+ 'car')
+console.log(4+"hi")
+console.log(4*'hi')
+console.log("hello".toUpperCase());
+console.log("HELLO".toLowerCase());
+console.log("hello".slice(1));
+console.log("hello".slice(1,3));
+let name = "John"
+console.log(name)
+console.log(name==='john')
+name = "kevin"
+console.log(name)
+let myFirstName= "frank"
+let myLastName= "Dice"
+let test 
+console.log(test)
+if (20 === 2) {
+    console.log("they are equal")
+} else {
+    console.log("not equal")
+}
+let num = 20
+if(num===20){
+    console.log("equals 20")
+}else if (num > 20){
+    console.log(greater)
+}else{
+    console.log("less than")
+}
+if (0){
+    console.log(false)
+}else{
+    console.log(true)
+}
