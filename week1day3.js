5d3485d04b7341a021fae63e5c81e7c70a6a0bc4
Jael Rodriguez <jaelrodriguez@Jaels-MacBook-Pro.local>
2022-09-28 19:27:34 -0400

    NOTES

--- week1day3.js
+++ week1day3.js
@@ -1,14 +1,31 @@
-console.log("1")
-console.log("2")
-console.log("3")
-console.log("4")
-console.log("5")
-console.log("6")
-console.log("7")
-console.log("8")
-console.log("9")
-console.log("10")
+// console.log("1")
+// console.log("2")
+// console.log("3")
+// console.log("4")
+// console.log("5")
+// console.log("6")
+// console.log("7")
+// console.log("8")
+// console.log("9")
+// console.log("10")
 
-for(let i = 1; i <= 10; i++) {
-    console.log(i);
+// for(let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+// console.log('after the loop')
+
+function printRange(start,end){
+    for (let i = start; i <= end; i++){
+        console.log(i)
+    }
 }
+// printRange(3,10);
+
+function printRange1(start,end) {
+    let i = start;
+    while (i<=end){
+        console.log (i);
+        i++
+    }
+}
+printRange1(7,21)
