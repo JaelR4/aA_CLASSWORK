94b235c6195cd07feea1981d99a385077a938864
Jael Rodriguez <jaelrodriguez@Jaels-MacBook-Pro.local>
2022-09-29 19:23:42 -0400

    NOTES

--- day4.js
+++ day4.js
@@ -1,13 +1,49 @@
-// let arr = ['c', 'a', 't']
-// let str = "car"
-// str [0] = "x"
-// console.log(str)
+// // let arr = ['c', 'a', 't']
+// // let str = "car"
+// // str [0] = "x"
+// // console.log(str)
 
-// arr[0]="x"
-// console.log(arr)
+// // arr[0]="x"
+// // console.log(arr)
 
-let arr=['hi', 10, 9, true, undefined, null, [1,2]]
-console.log(arr)
+// // let arr=['hi', 10, 9, true, undefined, null, [1,2]]
+// // console.log(arr)
 
-console.log(arr[0])
-console.log(arr[6][0])
+// // console.log(arr[0])
+// // console.log(arr[6][0])
+
+// let fruits = ["apple", "orange", "pear"]
+// fruits.length
+// console.log(fruits.indexOf("pear"))
+// console.log(fruits.includes ("watermelon"))
+
+// fruits.push('watermelon')
+// console.log(fruits)
+// fruits.pop()
+// console.log(fruits)
+// fruits.unshift("grape")
+// console.log(fruits)
+
+let numbers = [1,2,3,4,5,6,7,8,9,10]
+// console.log(fruits.concat(numbers))
+// console.log(fruits)
+
+// console.log(numbers.slice())
+// console.log(numbers.slice(1))
+// console.log(numbers.slice(0,2))
+// console.log(numbers.slice(5,10))
+
+let string = "hi my name is xander"
+console.log(string.split(" "))
+console.log(string)
+console.log(numbers.join("---"))
+
+for (let i=0; i<numbers.length; i++) {
+    let ele=numbers[i]
+    console.log(ele)
+}
+// for (let i = numbers.length-1; i>= 0;, i--){
+//     let ele= numbers [i]
+//     console.log(ele)
+
+// }
