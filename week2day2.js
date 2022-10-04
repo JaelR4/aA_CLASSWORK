aa10e6d335fc43517c684d91a136f73d2e836e4d
Jael Rodriguez <jaelrodriguez@Jaels-MacBook-Pro.local>
2022-10-04 19:23:11 -0400

    NOTES

--- week2day2.js
+++ week2day2.js
@@ -8,12 +8,55 @@
 // }
 // console.log(catBuilder("whiskers", "black" , "toys"))
 
-function catBuilder(name, color, toys){
-    return{
-        name: name,
-        color : color,
-        toys : toys,
+// function catBuilder(name, color, toys){
+//     return{
+//         name: name,
+//         color : color,
+//         toys : toys,
 
+//     }
+// }
+// console.log(catBuilder("whiskers", "black" , "toys"))
+
+// function laligatSum(num)
+
+// function isPrime(num){
+//     if (num < 2) return false
+//     for (let i=2; i < num; i++){
+//         if (num % i === 0) {
+//             return false
+//         }
+//     }
+//     return true;
+// }
+// function laligatSum(num){
+//     let sum = 0;
+//     for (let i = 2; i <= num; i++){
+//         if (isPrime(i)){
+//             sum +=i
+//         }
+//     }
+// return sum
+// }
+
+// console.log(laligatSum(10))
+// console.log(laligatSum(17))
+
+let cat = {
+    name : "whiskers",
+    color: "black",
+    toys :{
+        outside:['ball', 'whisthle'],
+        inside:['blanket']
     }
 }
-console.log(catBuilder("whiskers", "black" , "toys"))
+cat ["name"] = 'Jael'
+cat["age"] = "21"
+ console.log(cat)
+ console.log(cat["color"])
+        
+ 
+ console.log(cat.test) // UNDEFINED
+ let test = "name"
+ console.log(cat[test])
+ console.log(cat.test)
